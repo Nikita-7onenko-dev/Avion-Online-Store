e7a960adbcbf84fb47b70441bd32a3752d9d95ee
Nikita-7onenko-dev <nikitatrihomkin@gmail.com>
2025-09-13 02:19:31 +0300

    fix emailActivationMessage layout

--- src/Components/EmailActivationMessage/EmailActivationMessage.tsx
+++ src/Components/EmailActivationMessage/EmailActivationMessage.tsx
@@ -4,8 +4,10 @@ import styles from './emailActivationMessage.module.scss'
 export default function EmailActivationMessage(): React.JSX.Element {
 
   return (
-        <div className={styles.activationMessage}>
-          <p>We have sent an activation link to your email. Please check your inbox and follow the link</p>
-        </div> 
+    <div className={styles.container}>
+      <div className={styles.activationMessage}>
+        <p>We have sent an activation link to your email. Please check your inbox and follow the link</p>
+      </div>       
+    </div>
   )
 }
